3d5e5d4471bcf8edeb5123e8764e22939e15c01f
Tony Leigh <tonleigh@cisco.com>
2017-04-25 08:59:58 +0300

    excluding files from package

--- package.sh
+++ package.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 rm helloworld.tar.gz
-tar -czf  helloworld.tar.gz --exclude=node_modules .
+tar -czf  helloworld.tar.gz --exclude=node_modules --exclude=app_pid.txt --exclude=*.tar.gz .
